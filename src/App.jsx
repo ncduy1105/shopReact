95b83c81484a34c9fce31f8c32ef6bf12ffe124e
Luka Gurdulic <gurdulicluka@gmail.com>
2023-01-31 20:24:50 +0100

    Add Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
-import Sidebar from "./components/Sidebar";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
 
 const App = () => {
   return (
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+
+import woman from "../img/woman_hero.png";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section className="h-[800px] bg-hero bg-cover bg-center bg-no-repeat">
+      <div className="container relative mx-auto flex h-full">
+        {/* text */}
+        <div className="relative z-10 self-center">
+          <div className="flex w-fit items-center">
+            <div className="mr-3 h-[3px] w-[40px] rounded-full bg-red-500" />
+            <span className="font-medium">NEW TREND</span>
+          </div>
+          <h1 className="text-[70px] font-light leading-[1.1]">
+            AUTUMN SALE STYLISH
+            <br />
+            <span className="font-semibold">WOMENS</span>
+          </h1>
+          <Link
+            to="/"
+            className="mt-3 inline-block font-semibold underline decoration-2 underline-offset-4"
+          >
+            DISCOVER MORE
+          </Link>
+        </div>
+        {/* img */}
+        <div className="absolute bottom-0 right-[5vw] 3xl:right-[15vw]">
+          <img className="" src={woman} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
